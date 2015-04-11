f77a2bee39c103906202ea43a6b000b96659b3a7
Joey Poon <joey@joeypoon.com>
2015-04-11 00:27:13 -0500

    Refactor

---
+++ aid_calculations.rb
@@ -0,0 +1,491 @@
+class AidCalculations
+
+  def initialize
+    #2014-2015 Annual Limits
+    @dep1sub = 3500
+    @dep3total = 5500
+    @dep2sub = 4500
+    @dep2total = 6500
+    @dep3sub = 5500
+    @dep3total = 7500
+    @ind1sub = 3500
+    @ind1total = 9500
+    @ind2sub = 4500
+    @ind2total = 10500
+    @ind3sub = 5500
+    @ind3total = 12500
+    @grad_total = 20500
+    @annual_pell = 5730
+
+    #2014-2015 Aggregate Limits
+    @sub_agg = 23000
+    @loan_agg = 57500
+    @grad_agg = 138500
+
+    #Default values
+    @pell_award = 0
+    @sub_award = 0
+    @unsub_award = 0
+    @mdtus = 0
+    @mdtut = 0
+    @outside_pell = 0
+    @outside_sub = 0
+    @outside_unsub = 0
+
+    @tasfa = "n"
+  end
+
+  def is_int? input
+    if (Integer(input) rescue nil) != input.to_i
+      return false
+    else
+      return true
+    end
+  end
+
+  def is_float? input
+    if (Float(input) rescue nil) != input.to_f
+      return false
+    else
+      return true
+    end
+  end
+
+  def ask_tasfa
+    begin
+      puts "\nIs student TASFA? (y/n): "
+      @tasfa = gets.chomp
+    end until @tasfa == "y" || @tasfa == "n"
+  end
+
+  def is_tasfa?
+    if @tasfa = "y"
+      return true
+    else
+      return false
+    end
+  end
+
+  def ask_dependency_status
+    begin
+      puts "\nPlease enter dependency status (dep/ind): "
+      @dependency_status = gets.chomp
+    end until @dependency_status == "ind" || @dependency_status == "dep"
+  end
+
+  def ask_old_budget
+    begin
+      puts "\nPlease enter old budget: "
+      @old_budget = gets.chomp
+    end until is_int? @old_budget
+      @old_budget = @old_budget.to_i
+  end
+
+  def ask_old_efc
+    begin
+      puts "\nPlease enter old EFC: "
+      @old_efc = gets.chomp
+    end until is_int? @old_efc
+    @old_efc = @old_efc.to_i
+  end
+
+  def ask_pell_efc
+    begin
+      puts "\nPlease enter Pell EFC: "
+      @pell_efc = gets.chomp
+    end until is_int? @pell_efc
+    @pell_efc = @pell_efc.to_i
+  end
+
+  def ask_sap
+    begin
+      puts "\nPlease enter SAP status (good/bad): "
+      @sap_status = gets.chomp
+    end until @sap_status == "good" || @sap_status == "bad"
+  end
+
+  def is_sap?
+    if @sap_status == "bad"
+      return true
+    else
+      return false
+    end
+  end
+
+  def ask_fs_pell
+    begin
+      puts "\nPlease enter Fall + Spring Pell: "
+      @fs_pell = gets.chomp
+    end until is_int? @fs_pell
+    @fs_pell = @fs_pell.to_i
+  end
+
+  def ask_fs_sub
+    begin
+      puts "\nPlease enter Fall + Spring Sub: "
+      @fs_sub = gets.chomp
+    end until is_int? @fs_sub
+    @fs_sub = @fs_sub.to_i
+  end
+
+  def ask_fs_unsub
+    begin
+      puts "\nPlease enter Fall + Spring Unsub: "
+      @fs_unsub = gets.chomp
+    end until is_int? @fs_unsub
+    @fs_unsub = @fs_unsub.to_i
+
+  def ask_fall_enrollment
+    begin
+      puts "\nPlease enter Fall enrollment: "
+      @fall_enrollment = gets.chomp
+    end until is_int? @fall_enrollment
+    @fall_enrollment = @fall_enrollment.to_i
+  end
+
+  def ask_spring_enrollment
+    begin
+      puts "\nPlease enter Spring enrollment: "
+      @spring_enrollment = gets.chomp
+    end until is_int? @spring_enrollment
+    @spring_enrollment = @spring_enrollment.to_i
+  end
+
+  def ask_leu
+    begin
+      puts "\nPlease enter LEU: "
+      @pell_leu = gets.chomp
+    end until is_float?? @pell_leu
+    @pell_leu = @pell_leu.to_f
+  end
+
+  def ask_total_sub_borrowed
+    begin
+      puts "\nPlease enter total Sub borrowed: "
+      @total_sub_borrowed = gets.chomp
+    end until is_int? @total_sub_borrowed
+    @total_sub_borrowed = @total_sub_borrowed.to_i
+  end
+
+  def ask_total_unsub_borrowed
+    begin
+      puts "\nPlease enter total Unsub borrowed: "
+      @total_unsub_borrowed = gets.chomp
+    end until is_int? @total_unsub_borrowed
+    @total_unsub_borrowed = @total_unsub_borrowed.to_i
+  end
+
+  def set_total_loans_borrowed
+    @total_loans_borrowed = @total_sub_borrowed + @total_unsub_borrowed
+  end
+
+  def ask_outside_pell
+    begin
+      puts "\nEnter amount of outside Pell: "
+      @outside_pell = gets.chomp
+    end until is_int? @outside_pell
+    @outside_pell = @outside_pell.to_i
+  end
+
+  def ask_outside_sub
+    begin
+      puts "\nEnter amount of outside sub: "
+      @outside_sub = gets.chomp
+    end until is_int? @outside_sub
+    @outside_sub = @outside_sub.to_i
+  end
+
+  def ask_outside_unsub
+    begin
+      puts "\nEnter amount of outside unsub: "
+      @outside_unsub = gets.chomp
+    end until is_int? @outside_unsub
+    @outside_unsub = @outside_unsub.to_i
+  end
+
+  def check_enrollment
+    if @fall_enrollment == 0 || @spring_enrollment == 0
+      puts "\n***Please check NSLDS and add student to TM***"
+      ask_outside_pell
+      ask_outside_sub
+      ask_outside_unsub
+    end
+  end
+
+  def check_leu
+    if @pell_leu >= 500
+      puts "\n***Please confirm Pell LEU on NSLDS***"
+      begin
+        puts "LEU on NSLDS: "
+        @pell_leu = gets.chomp
+      end until is_float?? @pell_leu
+      @pell_leu = @pell_leu.to_f
+    end
+  end
+
+  def get_pell_leu
+    return @pell_leu
+  end
+
+  def check_loan_limits
+    if @total_loans_borrowed > @loan_agg || @total_sub_borrowed > @sub_agg
+      puts "\n***Loan limits exceeded***"
+      gets
+    end
+  end
+
+  def ask_summer_budget
+    begin
+      puts "\nPlease enter Summer budget: "
+      @summer_budget = gets.chomp
+    end until is_int? @summer_budget
+    @summer_budget = @summer_budget.to_i
+  end
+
+  def get_summer_budget
+    return @summer_budget
+  end
+
+  def set_new_budget
+    @new_budget = @old_budget + @summer_budget
+  end
+
+  def ask_new_efc
+    begin
+      puts "\nPlease enter new EFC: "
+      @new_efc = gets.chomp
+    end until is_int? @new_efc
+    @new_efc = @new_efc.to_i
+  end
+
+  def check_efc
+    while @new_efc < @old_efc
+      puts "\nNew EFC must be larger than or equal to old EFC."
+      ask_old_efc
+      ask_new_efc
+    end
+  end
+
+  def set_summer_efc
+    @summer_efc = @new_efc - @old_efc
+  end
+
+  def get_summer_efc
+    return @summer_efc
+  end
+
+  def ask_grade_level
+    begin
+      puts "\nPlease enter student grade level (1, 2, 3, 4, 5, or 6): "
+      @grade_level = gets.chomp.to_i
+    end until @grade_level >= 1 && @grade_level <= 6
+  end
+
+  def calculate_awards
+    max_mdtu = 1500
+    if !is_tasfa?
+      calculate_pell
+      calculate_mdtus
+      calculate_sub
+      calculate_unsub
+      check_over_summer_budget
+    else
+      @mdtut = max_mdtu
+    end
+  end
+
+  def get_mdtut
+    return @mdtut
+  end
+
+  def display_awards
+    if @tasfa == "n"
+      puts "\nPell: " + @pell_award.to_s
+      puts "MDTUS: " + @mdtus.to_s
+    else
+      puts "MDTUT: " + @mdtut.to_s
+    end
+    puts "Sub Loans: " + @sub_award.to_s
+    puts "Unsub Loans: " + @unsub_award.to_s
+  end
+
+  def display_stats
+    puts "\nFill out sum app form: "
+    puts "SAP: " + @sap_status.to_s + "\nFall + Spring Pell: " + @fs_pell.to_s + "\nNew Budget: " + @new_budget.to_s + "\nOld Budget: " + @old_budget.to_s + "\nSummer Budget: " + @summer_budget.to_s + "\nNew EFC: " + @new_efc.to_s + "\nOld EFC: " + @old_efc.to_s + "\nSummer EFC: " + @summer_efc.to_s + "\nSummer Pell: " + @pell_award.to_s + "\nSummer Sub: " + @sub_award.to_s + "\nSummer Unsub: " + @unsub_award.to_s + "\n@mdtus: " + @mdtus.to_s
+  end
+
+  private
+
+  def calculate_pell
+    no_pell_threshold = 5157  #EFC threshold for no Pell
+    lowest_pell_threshold = 5101  #Bottom EFC for lowest Pell award bracket
+    lowest_pell = 602  #Lowest Pell for 1415 AY
+    lower_pell_bound = 100  #Lower bound of EFC bracket for regular Pell calc
+    upper_pell_bound = 5100  #Upper bound of EFC bracket for regular Pell calc
+    if @fall_enrollment + @spring_enrollment >= 24 || @grade_level > 4
+      @pell_award = 0
+    else
+      if @pell_efc > no_pell_threshold
+        @annual_pell = 0
+      elsif @pell_efc >= lowest_pell_threshold && @pell_efc <= no_pell_threshold
+        @annual_pell = lowest_pell
+      elsif @pell_efc > lower_pell_bound && @pell_efc <= upper_pell_bound
+        pell_factor = (@pell_efc - 1) / 100
+        @annual_pell = @annual_pell - (50 + pell_factor * 100)
+      elsif @pell_efc > 0 && @pell_efc <= lower_pell_bound
+        @annual_pell = @annual_pell - 50
+      end
+
+      if @pell_leu > 575  #Check if less than a quarter of annual pell remaining
+        @pell_award = ((600 - @pell_leu) * 0.01) * @annual_pell
+      else
+        @pell_award = @annual_pell - (@fs_pell + @outside_pell)
+        if @pell_award > @annual_pell / 4
+          @pell_award = @annual_pell / 4
+        end
+      end
+    end
+  end
+
+  def calculate_mdtus
+    award_threshold = 1000
+    max_mdtu = 1500
+    if @pell_award < award_threshold
+      @mdtus = max_mdtu - @pell_award
+    else
+      @mdtus = 0
+    end
+  end
+
+  def set_annual_loan_limits
+    if @dependency_status == "ind"
+      case @grade_level
+      when 1
+        @annual_sub_limit = @ind1sub
+        @annual_loan_limit = @ind1total
+      when 2
+        @annual_sub_limit = @ind2sub
+        @annual_loan_limit = @ind2total
+      when 3..5
+        @annual_sub_limit = @ind3sub
+        @annual_loan_limit = @ind3total
+      when 6
+        @annual_sub_limit = 0
+        @annual_loan_limit = @grad_total
+      end
+    else
+      case @grade_level
+      when 1
+        @annual_sub_limit = @dep1sub
+        @annual_loan_limit = @dep3total
+      when 2
+        @annual_sub_limit = @dep2sub
+        @annual_loan_limit = @dep2total
+      else
+        @annual_sub_limit = @dep3sub
+        @annual_loan_limit = @dep3total
+      end
+    end
+  end
+
+  def calculate_sub
+    if @fs_sub > 0 && @fs_sub + @outside_sub < @annual_sub_limit
+      if @fs_sub + @outside_sub + @fs_unsub + @outside_unsub < @annual_loan_limit
+        @sub_award = @annual_sub_limit - (@fs_sub + @outside_sub)
+        if @sub_award > @annual_sub_limit / 2
+          @sub_award = @annual_sub_limit / 2
+        end
+        if @total_sub_borrowed + @sub_award >= @sub_agg
+          @sub_award = @sub_agg - @total_sub_borrowed
+        end
+
+      else
+        @sub_award = 0
+      end
+
+    else
+      @sub_award = 0
+    end
+  end
+
+  def set_total_awards
+    @total_awards = @pell_award + @sub_award + @unsub_award + @mdtus + @mdtut
+  end
+
+  def calculate_unsub
+    if @fs_unsub > 0 && @fs_unsub < @annual_loan_limit - (@fs_sub + @outside_sub)
+      @unsub_award = @annual_loan_limit - (@sub_award + @fs_sub + @outside_sub + @fs_unsub + @outside_unsub)
+      if @unsub_award > @annual_loan_limit/2
+        @unsub_award = @annual_loan_limit/2 - @sub_award
+      end
+      if @total_loans_borrowed + @sub_award + @unsub_award >= @loan_agg
+        @unsub_award = @loan_agg - (@total_loans_borrowed + @sub_award)
+      end
+
+      set_total_awards
+
+      if @total_awards > @summer_budget
+        @unsub_award -= (@total_awards - @summer_budget)
+      end
+
+    else
+      @unsub_award = 0
+    end
+  end
+
+  def check_unsub_over_budget
+    if @total_awards > @summer_budget - @summer_efc
+      if @unsub_award > 0
+        @unsub_award -= (@total_awards - (@summer_budget - @summer_efc))
+        if @unsub_award < 0
+          @unsub_award = 0
+        end
+      end
+      set_total_awards
+    end
+  end
+
+  def check_sub_over_budget
+    if @total_awards > @summer_budget - @summer_efc
+      if @sub_award > 0
+        @sub_award -= (@total_awards - (@summer_budget - @summer_efc))
+        if @sub_award < 0
+          @sub_award = 0
+        end
+      end
+      set_total_awards
+    end
+  end
+
+  def check_mdtus_over_budget
+    if @total_awards > @summer_budget - @summer_efc
+      if @mdtus > 0
+        @mdtus -= (@total_awards - (@summer_budget - @summer_efc))
+        if @mdtus < 0
+          @mdtus = 0
+        end
+      end
+      set_total_awards
+    end
+  end
+
+  def check_pell_over_budget
+    if @total_awards > @summer_budget - @summer_efc
+      if @pell_award > 0
+        @pell_award -= (@total_awards - (@summer_budget - @summer_efc))
+        if @pell_award < 0
+          @pell_award = 0
+        end
+      end
+      set_total_awards
+    end
+  end
+
+  def check_over_summer_budget
+    check_unsub_over_budget
+    check_sub_over_budget
+    check_mdtus_over_budget
+    check_pell_over_budget
+  end
+
+end
--- sum_calc.rb
+++ sum_calc.rb
@@ -1,412 +1,61 @@
-#2014-2015 Annual Limits
-dep1sub = 3500
-dep1Total = 5500
-dep2sub = 4500
-dep2Total = 6500
-dep3sub = 5500
-dep3Total = 7500
-ind1sub = 3500
-ind1Total = 9500
-ind2sub = 4500
-ind2Total = 10500
-ind3sub = 5500
-ind3Total = 12500
-gradTotal = 20500
-annualPell = 5730
+require "AidCalculations"
 
-#2014-2015 Aggregate Limits
-subAgg = 23000
-loanAgg = 57500
-gradAgg = 138500
+puts "Enter 1 to continue or press any other key to exit"
+action = gets.chomp
 
-#Default values
-outsidePell = 0
-outsideSub = 0
-outsideUnsub = 0
+while action == "1"
 
-def validateInt input
-  if (Integer(input) rescue nil) != input.to_i
-    return false
-  else
-    return true
-  end
-end
+  student = AidCalculations.new
+  student.ask_tasfa
+  student.ask_dependency_status
+  student.ask_old_budget
+  student.ask_old_efc
+  student.ask_pell_efc
+  student.ask_sap
 
-def validateFloat input
-  if (Float(input) rescue nil) != input.to_f
-    return false
-  else
-    return true
-  end
-end
-
-begin
-
-  puts "Type 1 for new calculation or press any other key to exit."
-  action = gets.chomp
-
-  #ROASTAT
-  begin
-    puts "\nIs student TASFA? (y/n): "
-    tasfa = gets.chomp
-  end until tasfa == "y" || tasfa == "n"
-
-  begin
-    puts "\nPlease enter dependency status (dep/ind): "
-    depStatus = gets.chomp
-  end until depStatus == "dep" || depStatus == "ind"
-
-  begin
-    puts "\nPlease enter old budget: "
-    oldBudget = gets.chomp
-  end until validateInt oldBudget
-  oldBudget = oldBudget.to_i
-
-  begin
-    puts "\nPlease enter old EFC: "
-    oldEfc = gets.chomp
-  end until validateInt oldEfc
-  oldEfc = oldEfc.to_i
-
-  begin
-    puts "\nPlease enter Pell EFC: "
-    pellEfc = gets.chomp
-  end until validateInt pellEfc
-  pellEfc = pellEfc.to_i
-
-  begin
-    puts "\nPlease enter SAP status (good/bad): "
-    sapStatus = gets.chomp
-  end until sapStatus == "good" || sapStatus == "bad"
-
-  if sapStatus == "bad"
-    puts "\n***Please check student after SAP is reviewed - Add ROAMESG***"
+  if student.is_sap?
+    puts "\n***Please re-review application after SAP is reviewed***"
     redo
   end
 
-  #RPAAWRD
-  if tasfa == "n"
-    begin
-      puts "\nPlease enter Fall + Spring Pell: "
-      fsPell = gets.chomp
-    end until validateInt fsPell
-    fsPell = fsPell.to_i
-
-    begin
-      puts "\nPlease enter Fall + Spring Sub: "
-      fsSub = gets.chomp
-    end until validateInt fsSub
-    fsSub = fsSub.to_i
-
-    begin
-      puts "\nPlease enter Fall + Spring Unsub: "
-      fsUnsub = gets.chomp
-    end until validateInt fsUnsub
-    fsUnsub = fsUnsub.to_i
-
-  #ROAENRL
-    begin
-      puts "\nPlease enter Fall enrollment: "
-      fallEnrollment = gets.chomp
-    end until validateInt fallEnrollment
-    fallEnrollment = fallEnrollment.to_i
-
-    begin
-      puts "\nPlease enter Spring enrollment: "
-      springEnrollment = gets.chomp
-    end until validateInt springEnrollment
-    springEnrollment = springEnrollment.to_i
-
-  #RNASL
-    begin
-      puts "\nPlease enter LEU: "
-      pellLeu = gets.chomp
-    end until validateFloat pellLeu
-    pellLeu = pellLeu.to_f
-
-    begin
-      puts "\nPlease enter total Sub borrowed: "
-      totalSub = gets.chomp
-    end until validateInt totalSub
-    totalSub = totalSub.to_i
-
-    begin
-      puts "\nPlease enter total Unsub borrowed: "
-      totalUnsub = gets.chomp
-    end until validateInt totalUnsub
-    totalUnsub = totalUnsub.to_i
-
-    totalLoans = totalSub + totalUnsub
-
-    if fallEnrollment == 0 || springEnrollment == 0
-      puts "\n***Please check NSLDS and add student to TM***"
-
-      begin
-        puts "\nEnter amount of outside Pell: "
-        outsidePell = gets.chomp
-      end until validateInt outsidePell
-      outsidePell = outsidePell.to_i
-
-      begin
-        puts "\nEnter amount of outside sub: "
-        outsideSub = gets.chomp
-      end until validateInt outsideSub
-      outsideSub = outsideSub.to_i
-
-      begin
-        puts "\nEnter amount of outside unsub: "
-        outsideUnsub = gets.chomp
-      end until validateInt outsideUnsub
-      outsideUnsub = outsideUnsub.to_i
-
-    end
-
-    if pellLeu >= 500
-      puts "\n***Please confirm Pell LEU on NSLDS***"
-      begin
-        puts "LEU on NSLDS: "
-        pellLeu = gets.chomp
-      end until validateFloat pellLeu
-      pellLeu = pellLeu.to_f
-    end
-
-    if totalLoans > loanAgg || totalSub > subAgg
-      puts "\n***Loan limits exceeded***"
-      gets
-    end
+  if !student.is_tasfa?
+    student.ask_fs_pell
+    student.ask_fs_sub
+    student.ask_fs_unsub
   end
 
-  #RBAPBUD
-  begin
-    puts "\nPlease enter Summer budget: "
-    summerBudget = gets.chomp
-  end until validateInt summerBudget
-  summerBudget = summerBudget.to_i
+  student.ask_fall_enrollment
+  student.ask_spring_enrollment
 
-  newBudget = oldBudget + summerBudget
-
-  #RNIMS
-  begin
-    puts "\nPlease enter new EFC: "
-    newEfc = gets.chomp
-  end until validateInt newEfc
-  newEfc = newEfc.to_i
-
-  while newEfc < oldEfc
-    puts "\nNew EFC must be larger than or equal to old EFC."
-    begin
-      puts "\nPlease enter old EFC: "
-      oldEfc = gets.chomp
-    end until validateInt oldEfc
-    oldEfc = oldEfc.to_i
-
-    begin
-      puts "\nPlease enter new EFC: "
-      newEfc = gets.chomp
-    end until validateInt newEfc
-    newEfc = newEfc.to_i
+  if !student.is_tasfa?
+    student.ask_leu
+    student.ask_total_sub_borrowed
+    student.ask_total_unsub_borrowed
+    student.set_total_loans_borrowed
+    student.check_leu
+    student.check_loan_limits
   end
 
-  sumEfc = newEfc - oldEfc
+  student.check_enrollment
+  student.ask_summer_budget
+  student.set_new_budget  #optional
 
-  #WADVISE
-  begin
-    puts "\nPlease enter student grade level (1, 2, 3, 4, 5, or 6): "
-    gradeLevel = gets.chomp.to_i
-  end until gradeLevel >= 1 && gradeLevel <= 6
+  student.ask_new_efc
+  student.check_efc
+  student.set_summer_efc
+  student.ask_grade_level
 
-  #ROAUSDF
   puts "\n***Please input in ROAUSDF: LEU, Summer hours, budget, and EFC***"
-  puts "\nLEU: " + pellLeu.to_s + " \nSummer budget: " + summerBudget.to_s + " \nSummer EFC: " + sumEfc.to_s
+  puts "\nLEU: " + student.get_pell_leu.to_s + " \nSummer budget: " + student.get_summer_budget.to_s + " \nSummer EFC: " + student.get_summer_efc.to_s
   gets
 
-  #RPAAWRD
+  student.calculate_awards
+
   puts "\nStudent should be awarded: "
-  if tasfa == "n"
-    if fallEnrollment + springEnrollment >= 24
-      pellAward = 0
-    else
-      if pellEfc > 5157
-        annualPell = 0
-      elsif pellEfc >= 5101 && pellEfc <= 5157
-        annualPell = 602
-      elsif pellEfc > 100 && pellEfc <= 5100
-        pellFactor = (pellEfc - 1) / 100
-        annualPell = annualPell - (50 + pellFactor * 100)
-      elsif pellEfc > 0 && pellEfc <= 100
-        annualPell = annualPell - 50
-      end
+  student.display_awards
+  student.display_stats  #optional
 
-      if pellLeu > 575
-        pellAward = ((600 - pellLeu) * 0.01) * annualPell
-      else
-        pellAward = annualPell - (fsPell + outsidePell)
-        if pellAward > annualPell / 4
-          pellAward = annualPell / 4
-        end
-      end
-    end
-
-    if gradeLevel > 4
-      pellAward = 0
-    end
-
-    if pellAward < 1000
-      mdtus = 1500 - pellAward
-    else
-      mdtus = 0
-    end
-
-    if depStatus == "ind"
-      case gradeLevel
-      when 1
-        annualSubLim = ind1sub
-        annualLoanLim = ind1Total
-      when 2
-        annualSubLim = ind2sub
-        annualLoanLim = ind2Total
-      when 3..5
-        annualSubLim = ind3sub
-        annualLoanLim = ind3Total
-      when 6
-        annualSubLim = 0
-        annualLoanLim = gradTotal
-      end
-    else
-      case gradeLevel
-      when 1
-        annualSubLim = dep1sub
-        annualLoanLim = dep1Total
-      when 2
-        annualSubLim = dep2sub
-        annualLoanLim = dep2Total
-      else
-        annualSubLim = dep3sub
-        annualLoanLim = dep3Total
-      end
-    end
-
-    if fsSub > 0 && fsSub + outsideSub < annualSubLim
-      if fsSub + outsideSub + fsUnsub + outsideUnsub < annualLoanLim
-        subAward = annualSubLim - (fsSub + outsideSub)
-        if subAward > annualSubLim/2
-          subAward = annualSubLim/2
-        end
-        if totalSub + subAward >= subAgg
-          subAward = subAgg - totalSub
-        end
-
-      else
-        subAward = 0
-      end
-
-    else
-      subAward = 0
-    end
-
-    if fsUnsub > 0 && fsUnsub < annualLoanLim - (fsSub + outsideSub)
-      unsubAward = annualLoanLim - (subAward + fsSub + outsideSub + fsUnsub + outsideUnsub)
-      if unsubAward > annualLoanLim/2
-        unsubAward = annualLoanLim/2 - subAward
-      end
-      if totalLoans + subAward + unsubAward >= loanAgg
-        unsubAward = loanAgg - (totalLoans + subAward)
-      end
-
-      totalAwards = pellAward + subAward + unsubAward + mdtus
-
-      if totalAwards > summerBudget
-        unsubAward -= (totalAwards - summerBudget)
-      end
-
-    else
-      unsubAward = 0
-    end
-
-    totalAwards = pellAward + mdtus + subAward + unsubAward
-
-  else
-    pellAward = 0
-    mdtut = 1500
-    subAward = 0
-    unsubAward = 0
-    totalAwards = mdtut
-  end
-
-  if totalAwards > summerBudget - sumEfc
-    if unsubAward > 0
-      unsubAward -= (totalAwards - (summerBudget - sumEfc))
-      if unsubAward < 0
-        unsubAward = 0
-      end
-    end
-    totalAwards = pellAward + mdtus + subAward + unsubAward
-  end
-
-  if totalAwards > summerBudget - sumEfc
-    if subAward > 0
-      subAward -= (totalAwards - (summerBudget - sumEfc))
-      if subAward < 0
-        subAward = 0
-      end
-    end
-    totalAwards = pellAward + mdtus + subAward + unsubAward
-  end
-
-  while totalAwards > summerBudget - sumEfc
-    if mdtus > 0
-      mdtus -= (totalAwards - (summerBudget - sumEfc))
-      if mdtus < 0
-        mdtus = 0
-      end
-    end
-    totalAwards = pellAward + mdtus + subAward + unsubAward
-  end
-
-  while totalAwards > summerBudget - sumEfc
-    if pellAward > 0
-      pellAward -= (totalAwards - (summerBudget - sumEfc))
-      if pellAward < 0
-        pellAward = 0
-      end
-    end
-    totalAwards = pellAward + mdtus + subAward + unsubAward
-  end
-
-  puts "\nPell: " + pellAward.to_s
-  if tasfa == "n"
-    puts "MDTUS: " + mdtus.to_s
-  else
-    puts "MDTUT: " + mdtut.to_s
-  end
-  puts "Sub Loans: " + subAward.to_s
-  puts "Unsub Loans: " + unsubAward.to_s
-  if subAward + unsubAward + pellAward == 0 && mdtus > 0
-    puts "\n***Make sure to check award letter***"
-  end
-  gets
-
-  #RRAAREQ
-  if fallEnrollment == 0 && springEnrollment == 0
-    puts "\n***Please satisfy docs on RRAAREQ and add TC***"
-    gets
-  else
-    puts "\n***Please satisfy docs on RRAAREQ***"
-    gets
-  end
-
-  totalAwards = pellAward + subAward + unsubAward + mdtus
-
-  #ROAMESG
-  if totalAwards == 0
-    puts "\n***Please add ROAMESG***"
-    gets
-  end
-
-  puts "\nFill out sum app form: "
-  puts "SAP: " + sapStatus.to_s + "\nFall + Spring Pell: " + fsPell.to_s + "\nNew Budget: " + newBudget.to_s + "\nOld Budget: " + oldBudget.to_s + "\nSummer Budget: " + summerBudget.to_s + "\nNew EFC: " + newEfc.to_s + "\nOld EFC: " + oldEfc.to_s + "\nSummer EFC: " + sumEfc.to_s + "\nSummer Pell: " + pellAward.to_s + "\nSummer Sub: " + subAward.to_s + "\nSummer Unsub: " + unsubAward.to_s + "\nMDTUS: " + mdtus.to_s
-
-  puts "\n\nType 1 for new calculation or press any other key to exit."
+  puts "Enter 1 to continue or press any other key to exit"
   action = gets.chomp
 
-end until action != "1"
+end
